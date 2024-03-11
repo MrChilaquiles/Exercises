1d5556b1ba3b95bc39a2dbb31da2b710d93ed60e
Rogelio García <106293025+MrChilaquiles@users.noreply.github.com>
2024-03-10 22:07:21 -0600

    Add timer

--- Barquitos.c
+++ Barquitos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     int x, y, hs = 0;
@@ -8,9 +9,11 @@ int main() {
     printf("Bienvenido a BarquitosBattle!!!\n");
     printf("Para jugar necesitas introducir una coordenada como en el siguiente ejemplo:\nInserte una coordenada: 0,1\n");
     printf("Listo para la accion? Que comience la batalla!!!\n");
+    _sleep(5);
     while (hs < 4) {
+        system("cls");
         printf("Barcos restantes: %d\n", 4 - hs);
-        printf("Ingrese una coordenda: ");
+        printf("Ingrese una coordenada: ");
         scanf("%d,%d", &x, &y);
         if ((x < 0 || x > 1) || (y < 0 || y > 3)) {
             printf("La coordenada %d,%d no es valida\n", x, y);
@@ -21,6 +24,8 @@ int main() {
         } else {
             printf("Intentalo de nuevo\n");
         }
+        _sleep(3);
     }
+    system("cls");
     printf("Felicidades Pedrito me ganaste!!!");
 }
