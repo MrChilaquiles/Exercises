3a76a753bf35a75ef1b72823f44c2dec4e5717ff
Rogelio García <106293025+MrChilaquiles@users.noreply.github.com>
2024-03-12 22:22:19 -0600

    Tarea 6

---
+++ cont2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int cont = 5;
+    while (cont < 20) {
+        printf("prueba\n");
+        cont += 3;
+    }
+    return 8;
+}
---
+++ m90.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int m = 3;
+    while (m < 90) {
+        printf("%d\n", m);
+        m += 3;
+    }
+    return 8;
+}
