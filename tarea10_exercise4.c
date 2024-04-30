64a06209f1aa4453af21bdf4b3f4a1ab3de119a2
MrChilaquiles <RGGA2000@gmail.com>
2024-04-29 22:28:02 -0600

    Task10 - Functions.2

--- tarea10_exercise4.c
+++ tarea10_exercise4.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 void validar(char correo[]) {
-    char *arroba = strchr(correo, '@');
-    if (arroba != NULL) {
+    char *arroba = strchr(correo, '@'); //Se utiliza un puntero porque la funcion strchr devuelve un puntero
+    if (arroba != NULL) { //Si es diferente de NULL, significa que se encontro la arroba ya que la funcion strchr devuelve NULL si no se encuentra el caracter
         printf("La direccion de correo es valida");
     } else {
         printf("La direccion de correo es invalida");
