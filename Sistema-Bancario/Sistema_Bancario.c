0e8d18770a2c0d3ea39c911c64ba5a7c6c36338c
MrChilaquiles <RGGA2000@gmail.com>
2024-05-06 17:37:10 -0600

    First documentation

--- Sistema-Bancario/Sistema_Bancario.c
+++ Sistema-Bancario/Sistema_Bancario.c
@@ -7,46 +7,47 @@
 int numTarj = 0, numCel, saldo;
 char nombre[30], apellidos[30], numNIP[5];
 
-void limpiarBuffer() {
+void limpiarBuffer() { // Funcion para limpiar el buffer
     int c;
-    while ((c = getchar()) != '\n' && c != EOF) { }
+    while ((c = getchar()) != '\n' && c != EOF) { } // Mientras el caracter sea diferente a un salto de linea o fin de archivo se limpiara el buffer
 }
-void verificarDatos() {
+void verificarDatos() { // Funcion para verificar los datos del cliente
     int verificarCel = 0;
     char verificarNIP[5];
     bool validacionCel = false, validacionNIP = false;
-    while (validacionCel == false || validacionNIP == false) {
-        if (validacionCel == false) {
+    while (validacionCel == false || validacionNIP == false) { // Mientras validacionCel o validacionNIP sean falsos se ejecutara el ciclo
+        if (validacionCel == false) { // Si validacionCel es falso se ejecutara el siguiente bloque de codigo
             printf("Ingrese su numero celular: ");
             scanf("%d", &verificarCel);
-            limpiarBuffer();
-            if (verificarCel == numCel) {
-                validacionCel = true;
-            } else {
+            limpiarBuffer(); // Limpia el buffer
+            if (verificarCel == numCel) { // Si verificarCel es igual a numCel se ejecutara el siguiente bloque de codigo
+                validacionCel = true; 
+            } else { 
                 printf("Error: Numero incorrecto");
             }
         }
-        if (validacionNIP == false) {
+        if (validacionNIP == false) { // Si validacionNIP es falso se ejecutara el siguiente bloque de codigo
             printf("Ingrese su numNIP: ");
-            scanf("%s", &numNIP)
-            limpiarBuffer();
-            if (strcmp(numNIP, verificarNIP) == 0) {
+            scanf("%s", &numNIP);
+            limpiarBuffer(); // Limpia el buffer
+            strcpy(verificarNIP, numNIP); // Copia el valor de numNIP a verificarNIP esto se hace para comparar los valores
+            if (strcmp(numNIP, verificarNIP) == 0) { //strcmp compara dos cadenas de caracteres y si son iguales devuelve 0 en caso contrario devuelve un valor diferente de 0
                 validacionNIP = true;
             } else {
-                printf("Error: NIP incorrecto");
+                printf("Error: NIP incorrecto\n");
+                system("cls");
             }
         }
     }
-    printf("Datos ingresados correctamente!!!\nNombre completo: %s %s", nombre, apellidos);
+    printf("Datos ingresados correctamente!!!\nNombre completo: %s %s\n", nombre, apellidos); 
 }
-void validacionDatos(int numValidar, bool isOk) {
-    bool isOk = false;
+void validacionDatos(int numValidar, bool isOk) { // Funcion para validar los datos ingresados
     int validacionIsOk = 0;
-    if (numValidar <= saldo && numValidar > 0) {
+    if (numValidar <= saldo && numValidar > 0) { // Si numValidar es menor o igual a saldo y numValidar es mayor a 0 se ejecutara el siguiente bloque de codigo
         printf("Los datos ingresados son correctos?\n1. Si\n2. No");
         scanf("%d", &validacionIsOk);
         limpiarBuffer();
-        if (validacionIsOk == 1) {
+        if (validacionIsOk == 1) { // Si validacionIsOk es igual a 1 se ejecutara el siguiente bloque de codigo
             isOk = true;
         } else {
             printf("Ingrese los datos nuevamente");
@@ -57,53 +58,53 @@ void validacionDatos(int numValidar, bool isOk) {
         isOk = false;
     }
 }
-void registroCliente() {
+void registroCliente() { // Funcion para registrar a un nuevo cliente
     bool validacionNIP = false;
     system("cls");
-    printf("Ingrese su nombre: ");
-    fgets(nombre, 30, stdin);
-    printf("Ingrese sus apellidos: ");
+    printf("Ingrese su nombre: "); // Pide al usuario que ingrese su nombre
+    fgets(nombre, 30, stdin); //fgets se utiliza para leer una cadena de caracteres
+    printf("Ingrese sus apellidos: "); // Pide al usuario que ingrese sus apellidos
     fgets(apellidos, 30, stdin);
-    printf("Ingrese su celular: ");
+    printf("Ingrese su celular: "); // Pide al usuario que ingrese su numero de celular
     scanf("%d", &numCel);
     limpiarBuffer();
-    printf("Ingrese su numero de tarjeta: ");
+    printf("Ingrese su numero de tarjeta: "); // Pide al usuario que ingrese su numero de tarjeta
     scanf("%d", &numTarj);
     limpiarBuffer();
-    while (validacionNIP == false) {
+    while (validacionNIP == false) { // Mientras validacionNIP sea falso se ejecutara el siguiente bloque de codigo
         printf("Ingrese su numNIP: ");
         scanf("%s", &numNIP);
         limpiarBuffer();
-        if (strlen(numNIP) == 4) {
+        if (strlen(numNIP) == 4) { // Si la longitud de numNIP es igual a 4 se ejecutara el siguiente bloque de codigo
             validacionNIP = true;
         } else {
             printf("Error: Ingrese un numero valido\n");
         }
     }
     printf("Registro exitoso!!!");
-    saldo = 0;
+    saldo = 0; // Inicializa el saldo en 0
     getch();
     system("cls");
 }
-void deposito() {
+void deposito() { // Funcion para depositar dinero
     int monto = 0, validacionIsOk = 0;
     bool isOk = false;
     system("cls");
-    registroCliente();
-    while (isOk == false) {
+    verificarDatos(); // Llama a la funcion verificarDatos
+    while (isOk == false) { // Mientras isOk sea falso se ejecutara el siguiente bloque de codigo
         printf("Ingrese monto a depositar: ");
         scanf("%d", &monto);
         limpiarBuffer();
         printf("El monto ingresado esta correcto?\n1. Si\n2.No");
         scanf("%d", &validacionIsOk);
-        if (validacionIsOk == 1) {
+        if (validacionIsOk == 1) { // Si validacionIsOk es igual a 1 se ejecutara el siguiente bloque de codigo
             isOk = true;
         } else {
             printf("Ingrese el monto de nuevo");
         }
     }
     printf("Monto depositado: %d\nNumero de tarjeta: %d", monto, numTarj);
-    saldo += monto;
+    saldo += monto; // Suma el monto al saldo
     getch();
     system("cls");
 }
@@ -136,36 +137,36 @@ int main() {
     while (opc!=7) {
         printf("Sea bienvenido a su banco!!! \nIndique a continuacion la operacion a realizar:\n1. Registrar a un nuevo cliente\n2. Depositar dinero a cuenta propia\n3. Verificar saldo en tarjeta\n4. Transferir dinero a otras cuentas\n5. Recargar tiempo aire\n6. Cambiar numNIP\n7. Salir\nOpcion: ");
         scanf("%d", &opc);
-        limpiarBuffer();
+        limpiarBuffer(); // Limpia el buffer
         switch (opc) {
         case 1:
         fflush(stdin);
-            registroCliente();
+            registroCliente(); // Llama a la funcion registroCliente
             break;
         case 2:
-            deposito();
+            deposito(); // Llama a la funcion deposito
             break;
         case 3:
-            verSaldo();
+            verSaldo(); // Llama a la funcion verSaldo
             break;
         case 4: 
-            transDinero();
+            transDinero(); // Llama a la funcion transDinero
             break;
         case 5:
-            recarga();
+            recarga(); // Llama a la funcion recarga
             break;
         case 6:
-            cambioNIP();
+            cambioNIP(); // Llama a la funcion cambioNIP
             break;
         case 7:
             system("cls");
-            printf("Que tenga un buen dia!!!");
+            printf("Que tenga un buen dia!!!"); // Mensaje de despedida
             getch();
-            system("cls");
+            system("cls"); 
             break;
         default:
             system("cls");
-            printf("Ingrese una opcion valida");
+            printf("Ingrese una opcion valida"); // En caso de que el usuario ingrese una opcion invalida
             getch();
             system("cls");
             break;
