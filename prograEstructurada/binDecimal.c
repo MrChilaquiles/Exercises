44800997ffad724f722093fe34e15909b926c501
MrChilaquiles <RGGA2000@gmail.com>
2024-08-29 21:44:00 -0600

    Solving binDecimal

--- prograEstructurada/binDecimal.c
+++ prograEstructurada/binDecimal.c
@@ -2,13 +2,14 @@
 #include <math.h>
 
 int main() {
-    int bin[8], dec = 0, poten = 0;
+    int bin[8], dec = 0;
 
     for (int i = 0; i < 8; i++) {
         printf("Dame un numero para la posicion %d: ", i + 1);
         scanf("%d", &bin[i]);
-        for (int j = 1; j < 8; j++) {
-            poten = 2*j;
+        int poten = 1;
+        for (int j = 0; j < (7-i); j++) {
+            poten *= 2;
         }
         dec += bin[i]*poten;
     }
