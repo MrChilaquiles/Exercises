72b70596247006dcfc17216505f13013a16ae225
MrChilaquiles <RGGA2000@gmail.com>
2024-09-05 16:45:23 -0600

    Init program

---
+++ prograEstructurada/calMatrices.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, opc;
+    while (opc != 0) {
+        printf("Selecciona una opcion: \n1.- Suma de matrices\n2.- Resta de matrices\n3.- Multiplicacion de matrices\n4.- Division de matrices\n5.-Escalares de matrices\n0.- Salir");
+        scanf("%d", &opc);
+        switch (opc) {
+        case 1:
+            printf("");
+            break;
+        case 2:
+            printf("");
+            break;
+        case 3:
+            printf("");
+            break;
+        case 4:
+            printf("");
+            break;
+        case 5:
+            printf("");
+            break;
+        case 0:
+            printf("");
+            break;
+        default:
+            printf("Ingrese una opcion valida");
+            break;
+        }
+    }
+}
