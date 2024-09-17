2947f0170dbc7d41babaa9556529aaeebc6bbf3e
MrChilaquiles <RGGA2000@gmail.com>
2024-09-16 20:45:57 -0600

    Create new solution

---
+++ prograEstructurada/calMatricesModular.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define dim 10
+
+int main() {
+    int tamM, tamM1, tamN, tamN1, kEsc = 0, opc;
+    float matA[dim][dim], matB[dim][dim], matC[dim][dim];
+    do {
+        printf("Selecciona una opcion: \n1.- Suma de matrices\n2.- Resta de matrices\n3.- Multiplicacion por escalar\n4.- Multiplicacion de matrices\n0.- Salir\nOpcion: ");
+        scanf("%d", &opc);
+        switch (opc) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 0:
+            
+            break;
+        default:
+            break;
+        }
+    } while (opc != 0);
+}
