ad8a2cc3fc3e30ed4ecb350c0ff651e1495d678e
MrChilaquiles <RGGA2000@gmail.com>
2024-09-17 19:04:07 -0600

    Add new function

--- prograEstructurada/calMatricesModular.c
+++ prograEstructurada/calMatricesModular.c
@@ -8,7 +8,6 @@ void registro(int tamA, int tamB, float mat[dim][dim]){
         }
     }
 }
-
 void procedimiento(int tam, float matA[dim][dim], float matB[dim][dim], int opcP) {
     for (int i = 0; i < tam; i++) {
         for (int j = 0; j < tam; j++) {
@@ -22,7 +21,16 @@ void procedimiento(int tam, float matA[dim][dim], float matB[dim][dim], int opcP
         }
     }
 }
-
+void multMatrices(int tamA, int tamB, int tamC, float matA[dim][dim], float matB[dim][dim], float matC[dim][dim]){
+    for (int i = 0; i < tamA; i++) {
+        for (int j = 0; j < tamC; j++) {
+            matC[i][j] = 0;
+            for (int k = 0; k < tamB; k++) {
+                matC[i][j] += matA[i][k] * matB[k][j];
+            }
+        }
+    }
+}
 void resultado(int tamA, int tamB, float mat[dim][dim]) {
     for (int i = 0; i < tamA; i++) {
         for (int j = 0; j < tamB; j++) {
@@ -31,7 +39,6 @@ void resultado(int tamA, int tamB, float mat[dim][dim]) {
         printf("\n");
     }
 }
-
 int main() {
     int tamM, tamM1, tamN, tamN1, kEsc = 0, opc = 1, opcP;
     float matA[dim][dim], matB[dim][dim], matC[dim][dim];
@@ -82,10 +89,36 @@ int main() {
             }
             break;
         case 4:
-            printf("Gracias!! nos vemos luego :)");
+            printf("Ingrese un tamano n (0 < n < 10) para la fila de la matriz A: ");
+            scanf("%d", &tamM);
+            printf("Ingrese un tamano n (0 < n < 10) para la columna de la matriz A: ");
+            scanf("%d", &tamM1);
+            printf("Ingrese un tamano n (0 < n < 10) para la fila de la matriz B: ");
+            scanf("%d", &tamN);
+            printf("Ingrese un tamano n (0 < n < 10) para la columna de la matriz B: ");
+            scanf("%d", &tamN1);
+            if ((tamM > 0 && tamM < 10) && (tamM1 > 0 && tamM1 < 10) && (tamN > 0 && tamN < 10) && (tamN1 > 0 && tamN1 < 10)) {
+                if (tamM1 == tamN) {
+                    printf("A continuacion ingrese los valores para la matriz A\n");
+                    registro(tamM, tamM1, matA);
+                    printf("A continuacion ingrese los valores para la matriz B\n");
+                    registro(tamN, tamN1, matB);
+                    multMatrices(tamM, tamN, tamN1, matA, matB, matC);
+                    resultado(tamM, tamN1, matC);
+                } else if (tamN1 == tamM) {
+                    printf("A continuacion ingrese los valores para la matriz A\n");
+                    registro(tamM, tamM1, matA);
+                    printf("A continuacion ingrese los valores para la matriz B\n");
+                    registro(tamN, tamN1, matB);
+                    multMatrices(tamN, tamM, tamM1, matA, matB, matC);
+                    resultado(tamN, tamM1, matC);
+                } else {
+                    printf("No se puede realizar una multiplicacion de matrices con las dimensiones introducidas\n");
+                }
+            }
             break;
         case 0:
-            
+            printf("Gracias!! nos vemos luego :)");
             break;
         default:
             printf("Ingrese una opcion valida\n");
