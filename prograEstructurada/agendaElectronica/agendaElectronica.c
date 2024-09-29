811f88bac79270ef860cb6b3d2d22d6d2fdab8a6
MrChilaquiles <RGGA2000@gmail.com>
2024-09-29 15:31:38 -0600

    Add interface

--- prograEstructurada/agendaElectronica/agendaElectronica.c
+++ prograEstructurada/agendaElectronica/agendaElectronica.c
@@ -3,5 +3,32 @@
 #define MAXUsers 30
 
 int main() {
-    
+    int opc = 1, usercount = 0;
+    char dato[50], clear;
+    while (opc != 0) {
+        printf("Bienvenido a su agenda electronica\nSeleccione una opcion\n1.- Alta de usuario\n2.- Modificacion de usuario\n3.- Buscar usuario\n4.- Mostrar usuarios\n5.- Eliminar usuario\n0.- Salir");
+        scanf("%d", &opc);
+        switch (opc) {
+        case 1:
+            printf("Alta");
+            break;
+        case 2:
+            printf("Modificacion");
+            break;
+        case 3:
+            printf("buscar");
+            break;
+        case 4:
+            printf("mostrar todos");
+            break;
+        case 5:
+            printf("Eliminar");
+            break;
+        case 0:
+            printf("Gracias!! nos vemos luego :)");
+            break;
+        default:
+            break;
+        }
+    }
 }
