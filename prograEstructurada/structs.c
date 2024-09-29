3024c75b6e1e1d8e91696cd737b62c77ec27db69
MrChilaquiles <RGGA2000@gmail.com>
2024-09-29 15:32:08 -0600

    Delete \n

--- prograEstructurada/structs.c
+++ prograEstructurada/structs.c
@@ -9,23 +9,27 @@ typedef struct {
 
 int main() {
     persona a[3];
-    char nom[50], clear;
+    char nom[50];
+    char clear;
     for (int i = 0; i < 3; i++) {
     printf("ID: ");
     fgets(nom, sizeof(nom), stdin);
     do {clear = getchar();} while (clear != '\n');
+    nom[strlen(nom) - 1] = '\0';
     strcpy(a[i].id, nom);
     printf("Nombre: ");
     fgets(nom, sizeof(nom), stdin);
     do {clear = getchar();} while (clear != '\n');
+    nom[strlen(nom) - 1] = '\0';
     strcpy(a[i].nombres, nom);
     printf("Telefono: ");
     fgets(nom, sizeof(nom), stdin);
     do {clear = getchar();} while (clear != '\n');
+    nom[strlen(nom) - 1] = '\0';
     strcpy(a[i].telefono, nom);
     }
     for (int i = 0; i < 3; i++) {
-        printf("ID: %s\nNombres: %s\nTelefono: %s", a[i].id, a[i].nombres, a[i].telefono);
+        printf("ID: %sNombres: %sTelefono: %s", a[i].id, a[i].nombres, a[i].telefono);
     }
     return 0;
 }
