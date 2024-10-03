269122b3be03565acf0ad403552c34d41757e700
MrChilaquiles <RGGA2000@gmail.com>
2024-10-02 21:31:59 -0600

    Add limpiar

--- prograEstructurada/userId.c
+++ prograEstructurada/userId.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #define tam 30
 int main() {
     int idUser, edad;
     char nombre[tam], email[tam], clear;
+    size_t f;
     printf("Ingrese su ID: ");
     scanf("%d", &idUser);
     do {clear = getchar();} while (clear != '\n');
     printf("Ingrese su nombre: ");
     fgets(nombre, tam, stdin);
-    do {clear = getchar();} while (clear != '\n');
+    f = strlen(nombre);
+    if (nombre[strlen(nombre) - 1] == '\n') {
+        nombre[strlen(nombre) - 1] = '\0';
+    } else {
+        do {clear = getchar();} while (clear != '\n');
+    }
     printf("Ingrese su edad: ");
     scanf("%d", &edad);
     do {clear = getchar();} while (clear != '\n');
