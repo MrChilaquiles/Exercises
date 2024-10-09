68175230fb108eb13199e48a47e473e668bdac95
MrChilaquiles <RGGA2000@gmail.com>
2024-10-08 20:41:00 -0600

    Update file

--- Basicos/vector.cpp
+++ Basicos/vector.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <vector> // Incluir la cabecera vector
-
+#include <vector>
 using namespace std;
-
 int main() {
-    // Declarar un vector de enteros
-    vector<int> arr;
-
-    // Agregar elementos al vector
-    arr.push_back(1);
-    arr.push_back(2);
-    arr.push_back(3);
-
-    // Imprimir los elementos del vector
-    cout << "Elementos del vector: ";
-    for (int i = 0; i < arr.size(); i++) {
-        cout << arr[i] << " ";
+    vector<int> tareas(10);
+    vector<int> examenes(3);
+    vector<string> msj{"lala", "lalo"};
+    for (int i = 0; i < examenes.size(); i++) {
+        cin >> examenes[i];
     }
-    cout << endl;
-
+    for (int i = 0; i < examenes.size(); i++) {
+        cout << examenes[i];
+    }
     return 0;
 }
