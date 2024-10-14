15f5110e030bfed90ed14ccad1a07549110b733f
MrChilaquiles <RGGA2000@gmail.com>
2024-10-14 12:04:44 -0600

    Update vector inicial

--- Basicos/vector.cpp
+++ Basicos/vector.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
     vector<int> tareas(10);
-    vector<int> examenes(3);
+    vector<int> examenes(3, 100);
     vector<string> msj{"lala", "lalo"};
     for (int i = 0; i < examenes.size(); i++) {
         cin >> examenes[i];
